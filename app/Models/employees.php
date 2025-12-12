ac3801ed1963187612193b50a74def2a814f1754
Martin Doulík <123932205+robofancz@users.noreply.github.com>
2025-12-12 09:54:00 +0100

    [hotfix]

--- app/Models/employees.php
+++ app/Models/employees.php
@@ -4,32 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class employees extends Model
+public function position()
 {
-
-    use HasFactory;
-
-    public function position()
-    {
-        return $this->belongsTo(Position::class)
-    }
-
-    public function name()
-    {
-        return $this->belongsTo(Name::class)
-    }
-
-    public function UID()
-    {
-        return $this->belongsTo(User::class)
-    }
-
-    public function phone_number()
-    {
-        return $this->belongsTo(PhoneNumber::class)
-    }
-
-    protected $table = 'employees'; 
-
-
+    return $this->belongsTo(Position::class);
 }
+
+public function name()
+{
+    return $this->belongsTo(Name::class);
+}
+
+public function UID()
+{
+    return $this->belongsTo(User::class);
+}
+
+public function phone_number()
+{
+    return $this->belongsTo(PhoneNumber::class);
+}
+
