c03c6586be2b2069d0208ebb12b30a8fdec3c597
Martin Doulík <123932205+robofancz@users.noreply.github.com>
2025-12-12 09:56:25 +0100

    [hotfix]

--- app/Models/employees.php
+++ app/Models/employees.php
@@ -3,24 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Position;
+use App\Models\Name;
+use App\Models\User;
+use App\Models\PhoneNumber;
 
-public function position()
+class Employees extends Model
 {
-    return $this->belongsTo(Position::class);
-}
+    public function position()
+    {
+        return $this->belongsTo(Position::class);
+    }
 
-public function name()
-{
-    return $this->belongsTo(Name::class);
-}
+    public function name()
+    {
+        return $this->belongsTo(Name::class);
+    }
 
-public function UID()
-{
-    return $this->belongsTo(User::class);
-}
+    public function UID()
+    {
+        return $this->belongsTo(User::class);
+    }
 
-public function phone_number()
-{
-    return $this->belongsTo(PhoneNumber::class);
+    public function phone_number()
+    {
+        return $this->belongsTo(PhoneNumber::class);
+    }
 }
-
